8ba141ffaab9530f85eab8e81d859f5ecfa4ea90
misia97 <56821687+misia97@users.noreply.github.com>
2019-12-16 20:37:13 +0100

    php1

--- pages/formularz.php
+++ pages/formularz.php
@@ -37,6 +37,25 @@
                     <td><input type="radio" name="gender" value="K">K
                         <input type="radio" name="gender" value="M">M</td>
                 </tr>
+                <tr>
+                    <td>Aktualny miesiąc</td>
+                    <td><select name="month">
+                        
+                            <option value="1"> styczeń</option>
+                            <option value="2">luty </option>
+                            <option value="3">marzec</option>
+                            <option value="4"> kwiecień</option>
+                            <option value="5">maj </option>
+                            <option value="6"> czerwiec</option>
+                            <option value="7"> lipiec</option>
+                            <option value="8"> sierpień</option>
+                            <option value="9"> wrzesień</option>
+                            <option value="10">październik </option>
+                            <option value="11"> listopad</option>
+                            <option value="12">grudzień </option>
+                    </select>
+                    </td>
+                </tr>
 
             </table>
 
--- pages/wyniki.php
+++ pages/wyniki.php
@@ -8,14 +8,26 @@
 </head>
 
 <body>
-    <di class="container">
+    <div class="container">
    
 
         <?php 
-        define("amount", 6);
+        define("AMOUNT", 6);
     
         $dane;
             $error = false;
+            if(isset($_POST["month"])){
+                $month = $_POST["month"];
+                if($month != date('m')){
+                    die("<p>wykryto robota</p>");
+                }
+            }
+            else{
+                echo '<p>Nie wybrałeś miesiąca</p>';
+                $error = true;
+            }
+
+
             if (isset($_POST['firstName'])){
                 $name = $_POST['firstName'];
             }
@@ -26,7 +38,13 @@
 
             if(isset($_POST["phoneNumber"])){
                 $phoneNumber = $_POST["phoneNumber"];
-            }else{
+                $pattern = '/^[0-9]{9}$/';
+                if(preg_match($pattern,$phoneNumber) != 1){
+                    echo '<p>Numer telefonu jest niepoprawny</p>';
+                    $error = true;
+                }
+            }
+            else{
                 echo '<p>Nie podałeś numeru telefonu</p>';
                 $error = true;
             }
@@ -35,10 +53,11 @@
                 $email = $_POST["email"];
                 $pattern = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
                 if(preg_match($pattern,$email) != 1){
-                    echo '<p>Taki mail nie istnieje</p>';
+                    echo '<p>Email jest niepoprawny</p>';
                     $error = true;
                 }
-            }else{
+            }
+            else{
                 echo '<p>Nie podałeś email</p>';
                 $error = true;
             }
@@ -49,9 +68,9 @@
                 $interval =  date_diff( $birthDate, new DateTime(),true)->format('%R%a'); 
                 $interval = ((int)$interval); //cast string to int
 
-            if($interval < 365 * 18){
-                echo '<p>Nie masz ukończonych 18 lat</p>';
-            }
+                if($interval < 365 * 18){
+                    echo '<p>Nie masz ukończonych 18 lat</p>';
+                }
 
             }else{
                 echo '<p> Nie podałeś daty urodzenia</p>';
@@ -60,20 +79,24 @@
 
             if(isset($_POST["gender"])){
                 $gender = $_POST["gender"];
-            }else{
+            }
+            else{
                 echo '<p>Nie podałeś płci</p>';
                 $error = true;
             }
 
+           
+
             if(isset($_POST["numbers"])){
                 $numbers = $_POST["numbers"];
 
-                if (count($numbers) != constant("amount")){
+                if (count($numbers) != constant("AMOUNT")){
                     echo '<p>Nie wybrałeś 6 liczb</p>';
                     $error = true;
                 }
 
-            }else{
+            }
+            else{
                 echo '<p>Nie wybrałeś liczb</p>';
                 $error = true;
             }
@@ -98,26 +121,35 @@
         sort($drawn);
         sort($numbers);
         // echo implode(" ", $drawn);
-        echo "Wylosowane liczby ";
-        echo current($drawn) . " ";
+        echo "Wylosowane liczby: ";
+        echo current($drawn) . ", ";
         for($i = 1 , $cnt = count($drawn); $i < $cnt; $i++  ){
-            echo next($drawn) . " ";
+            echo next($drawn) . ", ";
         }
         reset($drawn);
         echo nl2br("\r\n");
         echo "Twoje liczby: ";
-        echo implode(" ", $numbers);
-        echo nl2br("\r\n");
+        echo implode(", ", $numbers);
+        echo nl2br("\r\n\r\n");
 
         // https://www.php.net/manual/en/language.operators.precedence.php
         ?>
 
-        <p> losowanie </p>
-        <?php echo"<p>Umiem napisać \" :) </p>" ; 
-        echo gettype($gender);
-        echo strcasecmp($gender, "K") == 0;
-        echo $gender == "K";
+        <h3> Aktualna temperatura atmosfery losowania to: </h3>
 
+        <?php 
+
+        $zmienna = 30 + '1.5';
+        echo "<h4>&nbsp;&nbsp;(konwersja) &nbsp;&nbsp;" . $zmienna;
+        echo nl2br("\r\n");
+        echo "(rzutowanie) &nbsp;&nbsp;&nbsp;" . (int) $zmienna;
+        echo nl2br("\r\n");
+
+
+        echo "A" . gettype($gender);
+        echo "B" . (strcasecmp($gender, "K") == 0);
+        echo "C" . (strcasecmp($gender, "K") === 0);
+        echo 'D' . ($gender == "K");
 
         $output = [];
         $output["title"] = $gender == "K" ? "Pani" : "Pan";
@@ -129,10 +161,31 @@
         
         echo "<h1> Trafiłeś ". end($output) ." </h1>";
 
+        $cos;
+        $rand = rand(0,1);
+        if ($rand)
+            $cos = $rand;
+        else
+            $cos = "Szczęście tym razem nie dopisało :(";
 
-        echo "<h3>" . reset($output) ." ". next($output) . " trafił " . end($output) . "<h3>";
-        echo "<h3> Nagroda zostanie wysłana na" . $output["email"] . " oraz " . $output["number"] . "</h3>";
-        echo "<h3> Wygrana nastąpiła na komputerze o ip " . $_SERVER['REMOTE_ADDR'] . "</h3>";
+        echo "<h2>" . $cos . " << to jest typu " . gettype($cos) . " (typowanie dynamiczne) </h2>";
+        echo nl2br("\r\n");
+
+
+        echo "<h3>" . reset($output) ." ". next($output) . " trafił\a " . end($output) . "</h3>";
+        echo "<h3> Nagroda zostanie wysłana na " . $output["email"] . " oraz " . $output["number"] . "</h3>";
+        echo "<h4> Proponowany adres e-mail: " . preg_replace('/(.+@.+[.])(.+)/', '$1lotto', $email) ."</h4>";
+
+        echo nl2br("\r\n");
+
+        echo "<h3> Wygrana nastąpiła na komputerze o IP " . $_SERVER['REMOTE_ADDR'] . "</h3>";
+        // print_r ($_SERVER);
+        echo "<p>Umiem napisać cudzysłów >> \" << :) </p>" ;    
+        
+        echo "<p>" . (2 + 2 * 2) . "</p>";
+
+        reset($output);
+        echo key($output);
 
         // print_r($_SERVER);
 
@@ -143,10 +196,9 @@
 
 
 
-
-
     <?php else : ?>
-        <p>Próbujesz oszukać. Aby zagrać w lotto, wejdź w <a href ="/laboratorium/pages/formularz.php">link</a></p>
+        <?php die("<p>Próbujesz oszukać. Aby zagrać w lotto, wejdź w <a href ='/laboratorium/pages/formularz.php'>link</a></p>"); ?>
+        <p>W O W</p>
     <?php endif; ?>
 
     
