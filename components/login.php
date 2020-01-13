531c5e61b9a683df371065373367ef5c9759e877
olbis97 <olbinski.dawid@interia.pl>
2020-01-13 15:38:10 +0100

    login component

--- components/login.php
+++ components/login.php
@@ -1,54 +1,71 @@
-<?php 
+<?php
 session_start();
 $uzytkownicy = [
     array('login' => 'user1', 'haslo' => sha1('ppp')),
     array('login' => 'user2', 'haslo' => sha1('ddd')),
     array('login' => 'user3', 'haslo' => sha1('fff'))
-    ];
-    $msg = '';
-function login(){
+];
+$msg = '';
+function login()
+{
     global $uzytkownicy;
     global $msg;
 
-        if(!isset($_SESSION['logged_in']) || empty($_SESSION['logged_in'])){
-            if(isset($_POST['submit'])){
-                if((!isset($_POST['login']) || empty($_POST['login'])) || (!isset($_POST['password']) || empty($_POST['password']))) {
-                    $msg = "Błąd logowania: Puste dane.";  
-                        promptMessage($msg);
-                    return false;
-                }
-                foreach($uzytkownicy as $user){
-                    if($user['login'] === $_POST['login'] && $user['haslo'] === sha1($_POST['password'])){
-                        $msg = "Witaj, {$user['login']}";
-                        $_SESSION['logged_in'] = true;
-                        $_SESSION['login'] = $user['login'];
-                        promptMessage($msg);
-                    return true;
-                    }
-                }
-                $msg = "Błąd logowania: Nieprawidlowe dane";
+    if (!isset($_SESSION['logged_in']) || empty($_SESSION['logged_in'])) { // jezeli nie jestes zalogowany
+
+        if (isset($_POST['submit'])) { // jezeli kliknales zaloguj czyli wejscie przez formularz logowania
+
+            if ((!isset($_POST['login']) || empty($_POST['login'])) || (!isset($_POST['password']) || empty($_POST['password']))) { // jezeli nie podales danych
+                $msg = "Błąd logowania: Puste dane.";
                 promptMessage($msg);
                 return false;
             }
-        }  elseif(isset($_POST['logout'])){
-            $_SESSION['logged_in'] = false;
-            $_SESSION['login'] = '';
-            $msg = "Wylogowano!";
-            promptMessage($msg);
-            return false;
-        }
 
+
+            include "database_connection.php";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $query = "SELECT * FROM `users` WHERE `login` = '{$_POST['login']}'";
+            $result = mysqli_query($conn, $query);
+            promptMessage($query);
+
+            if (mysqli_num_rows($result) > 0) {
+                $row = mysqli_fetch_assoc($result);
+                if ($row["password"] === sha1($_POST['password'])) {
+                    $msg = "Witaj, {$row['login']}";
+                    $_SESSION['logged_in'] = true;
+                    $_SESSION['login'] = $row['login'];
+                    promptMessage($msg);
+                    return true;
+                }
+            } 
+                $msg = "Błąd logowania: Nieprawidlowe dane";
+                promptMessage($msg);
+                return false;
+            mysqli_close($conn);
+        }
+    } elseif (isset($_POST['logout'])) {
+        $_SESSION['logged_in'] = false;
+        $_SESSION['login'] = '';
+        $msg = "Wylogowano!";
+        promptMessage($msg);
         return false;
     }
-    // login();
 
-    function promptMessage($message){
-        echo"
+    return false;
+}
+
+function promptMessage($message)
+{
+    echo "
             <script>
                 alert('{$message}');
             </script>
         
 
         ";
-    }
-?>
+}
--- pages/registry.php
+++ pages/registry.php
@@ -10,18 +10,46 @@
 <body>
 
     <?php
+    session_start();
 
     if (!isset($_POST["form_submitted"])) :
+        $user["name"] = "";
+        $user["last_name"] = "";
+        $user["birth_month"] = "";
+        $user["phone"] = "";
+        $user["email"] = "";
+        $user["login"] = "";
+        $user["password"] = "";
+        $user["id"] = "";
+
+
+        if (isset($_SESSION["logged_in"])) {
+            include "../components/database_connection.php";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $query = "SELECT * FROM `users` WHERE `login` = '{$_SESSION['login']}'";
+            $result = mysqli_query($conn, $query);
+
+            if (mysqli_num_rows($result) > 0) {
+                $user = mysqli_fetch_assoc($result);
+            }
+            mysqli_close($conn);
+        }
+
     ?>
         <h2>Formularz rejestracyjny</h2>
-        <form action="" method="post" autocomplete="on">
+        <form action="" method="post">
             <table>
                 <tr>
                     <td>
                         Imię
                     </td>
                     <td>
-                        <input type="text" name="name" autofocus><br />
+                        <input type="text" name="name" autofocus value=<?php echo $user["name"] ?>><br />
                     </td>
                 </tr>
                 <tr>
@@ -29,7 +57,7 @@
                         Nazwisko
                     </td>
                     <td>
-                        <input type="text" name="surname" required><br />
+                        <input type="text" name="surname" required value=<?php echo $user["last_name"] ?>><br />
                     </td>
                 </tr>
                 <tr>
@@ -37,7 +65,7 @@
                         Miesiąc urodzin
                     </td>
                     <td>
-                        <input list="month" name="birthmonth"><br />
+                        <input list="month" name="birthmonth" value=<?php echo $user["birth_month"] ?>><br />
                         <datalist id="month">
                             <option value="styczeń">
                             <option value="luty">
@@ -59,7 +87,7 @@
                         Telefon
                     </td>
                     <td>
-                        <input type="tel" name="phone" pattern="[0-9]{9}"><br />
+                        <input type="tel" name="phone" pattern="[0-9]{9}" value=<?php echo $user["phone"] ?>><br />
                     </td>
                 </tr>
                 <tr>
@@ -67,7 +95,15 @@
                         Email
                     </td>
                     <td>
-                        <input type="email" name="email" required> <br />
+                        <input type="email" name="email" required value=<?php echo $user["email"] ?>> <br />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Login
+                    </td>
+                    <td>
+                        <input type="text" name="login" required value=<?php echo $user["login"] ?>> <br />
                     </td>
                 </tr>
                 <tr>
@@ -84,6 +120,7 @@
             <input type="reset" value="Wyczyść">
             <input type="submit" value="Wyślij">
             <input type="hidden" name="form_submitted" value="1" />
+            <input type="hidden" name="id" value=<?php echo $user["id"] ?>>
         </form>
 
     <?php
@@ -97,25 +134,32 @@
             die("Connection failed: " . $conn->connect_error);
         }
 
-        
+
 
         $user["name"] = quotemeta($_POST["name"]);
         $user["surname"] = quotemeta($_POST["surname"]);
         $user["birthmonth"] = quotemeta($_POST["birthmonth"]);
         $user["phone"] = quotemeta($_POST["phone"]);
         $user["email"] = quotemeta($_POST["email"]);
-        $user["password"] = sha1($_POST["name"]);
+        $user["login"] = quotemeta($_POST["login"]);
+        $user["password"] = sha1($_POST["password"]);
+        $user["id"] = $_POST["id"];
+
+        if (isset($_POST["id"]) && empty($_POST["id"])) {
+            $query = "" .
+                sprintf("INSERT INTO `users` (`id`, `name`, `last_name`, `birth_month`, `phone`, `email`, `login`, `password`)
+             VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%s', '%s')", $user["name"], $user["surname"], $user["birthmonth"], $user["phone"], $user["email"], $user["login"], $user["password"]);
+        } else {
+            $query = sprintf("UPDATE `users` SET `name` = '%s', `last_name` = '%s', `birth_month` = '%s', `phone` = '%s', `email` = '%s', `login` = '%s', `password` = '%s' 
+            WHERE `users`.`id` = %d", $user["name"], $user["surname"], $user["birthmonth"], $user["phone"], $user["email"], $user["login"], $user["password"], $user["id"]);
+        }
 
 
-        
-        $query = "" .
-        sprintf("INSERT INTO `users` (`id`, `name`, `last_name`, `birth_month`, `phone`, `email`, `password`)
-         VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%s')",$user["name"],$user["surname"],$user["birthmonth"],$user["phone"],$user["email"],$user["password"] );
 
-        
         echo $query;
         if (mysqli_query($conn, $query)) {
             echo "New record created successfully";
+            echo $query;
         } else {
             echo "Error: " . $query . "<br>" . mysqli_error($conn);
         }
@@ -125,7 +169,7 @@
         // }
 
         mysqli_close($conn);
-        
+
 
 
     ?>
